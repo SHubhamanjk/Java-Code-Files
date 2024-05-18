88a33e559c246e66e38642d17307c7884a5f757c
ImShubh <147298263+SHubhamanjk@users.noreply.github.com>
2024-05-18 16:01:05 +0530

    Add files via upload

---
+++ firstprograme.java
@@ -0,0 +1,8 @@
+public class firstprograme {
+    public static void main(String args[]){
+        System.out.print("Hellow World!!\n");
+        System.out.println("Mai to papa hu !!!");
+        System.out.print("Is duniya ka papa");
+
+    }
+}
