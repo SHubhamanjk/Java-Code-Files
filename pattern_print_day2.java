124842f2de7f264e0a62fee8cd268879e60cdeda
ImShubh <147298263+SHubhamanjk@users.noreply.github.com>
2024-05-19 20:04:59 +0530

    Add files via upload

---
+++ pattern_print_day2.java
@@ -0,0 +1,16 @@
+public class pattern_print_day2 {
+    public static void main(String args[]){
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+
+
+    }
+    
+}
+
+
+// print is use to show the output on computer screen
+// println do the same and also create a new line
+// print with \n is also use to make a new line
